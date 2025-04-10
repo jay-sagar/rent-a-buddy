d13b513bf7270937db967262ffba99dabe2bb931
Jay Sagar <jaysagar871@gmail.com>
2025-04-10 20:09:19 +0530

    Add CORS configuration for frontend

--- backend/src/main/java/com/rentabuddy/backend/config/CorsConfig.java
+++ backend/src/main/java/com/rentabuddy/backend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://rent-a-buddy.vercel.app")
+                .allowedOrigins("https://rent-a-buddy.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
