60cee4e6a2415fd2dc4334d163103c9156b3fa10
Jay Sagar <jaysagar871@gmail.com>
2025-04-10 23:42:59 +0530

    Add improved CORS config

--- backend/src/main/java/com/rentabuddy/backend/controller/UserController.java
+++ backend/src/main/java/com/rentabuddy/backend/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "https://rent-a-buddy.vercel.app")
+@CrossOrigin
 public class UserController {
     @Autowired
     UserRepository userRepository;
