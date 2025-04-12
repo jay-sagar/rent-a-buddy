cf23e6ee6efbf6303bc5be67a9937e8648005797
Jay Sagar <jaysagar871@gmail.com>
2025-04-12 20:06:19 +0530

    Add corsorigin

--- backend/src/main/java/com/rentabuddy/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/rentabuddy/backend/config/WebMvcConfig.java
@@ -1,14 +1,14 @@
-package com.rentabuddy.backend.config;
-
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://rent-a-buddy.vercel.app/")
-                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS");
-    }
-}
+//package com.rentabuddy.backend.config;
+//
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://rent-a-buddy.vercel.app/")
+//                .allowedMethods("GET, POST, PUT, DELETE, OPTIONS");
+//    }
+//}
